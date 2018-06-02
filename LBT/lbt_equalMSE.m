c09c21977e3bf227a1be2cf11c12f2f53c413009
Basil Mustafa <BasilAMustafa@gmail.com>
2018-06-02 18:46:37 +0100

    start editing equal mse LBT scheme

--- LBT/lbt_equalMSE.m
+++ LBT/lbt_equalMSE.m
@@ -1,3 +1,18 @@
 % N is the number of layers, i is the size of the DCT transform
-function lbt_equalMSE(N, i);
+function Q = lbt_equalMSE(N, i)
+Q = zeros([N, i, i]);
+
+for n = 1:N
+    for r = 1:i
+        for c = 1:i
+            
+            
+            
+            
+        
+        end
+    end
+end
+
+end
 
